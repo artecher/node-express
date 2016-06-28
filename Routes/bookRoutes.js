c2ca4372c126bc783121c52fa6fd429fc8c4ab3b
ethan2014 <ethan.lee@sparkventures.co.nz>
2016-06-28 14:02:58 +1200

    [MASTER] Implement put, patch, delete and use middleware

--- Routes/bookRoutes.js
+++ Routes/bookRoutes.js
@@ -30,16 +30,47 @@ var routes = function(Book) {
       }
     });
 
+  bookRouter.use('/:bookId', function (req, res, next) {
+    Book.findById(req.params.bookId, function(err, book){
+      if(err) {
+        res.status(500).send(err);
+      }else if(book){
+        req.book = book;
+        next();
+      }else {
+        res.status(404);
+      }
+    })
+  });
   bookRouter.route('/:bookId')
     .get(function(req, res){
-      Book.findById(req.params.bookId, function(err, book){
-        if(err) {
+      res.json(req.book);
+    })
+    .put(function (req, res) {
+      req.book.title = req.body.title;
+      req.book.author = req.body.author;
+      req.book.genre = req.body.genre;
+      req.book.read = req.body.read;
+      req.book.save();
+      res.json(req.book);
+    })
+    .patch(function (req, res) {
+      req.book.title = req.body.title || req.book.title;
+      req.book.author = req.body.author || req.book.author;
+      req.book.genre = req.body.genre || req.book.genre;
+      req.book.read = req.body.read || req.book.read;
+      req.book.save();
+      res.json(req.book);
+    })
+    .delete(function (req, res) {
+      req.book.remove(function (err) {
+        if (err){
           res.status(500).send(err);
-        }else {
-          res.json(book);
+        } else {
+          res.status(204).send('Removed');
         }
-      })
-    });
+      });
+    })
 
   return bookRouter;
 };
